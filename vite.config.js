4fa1e3f0c89a9c9db81ed71f3c6489c259814911
Clay <150019517+Zedent-lab@users.noreply.github.com>
2025-06-24 20:08:01 +0300

    Update base path in Vite config for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Tales-House/', // <-- Base path for GitHub Pages deployment
+  base: '/', // <-- Base path for GitHub Pages deployment
   plugins: [react()],
 })
 
